11bf7e32eeac86a77fd3fd735ad836d63c375f68
venu-ibm <komativenugopalakrishna@gmail.com>
2024-07-15 15:47:18 +0530

    Update deploy-apis_1.sh

--- APICv10/Common/deploy-apis_1.sh
+++ APICv10/Common/deploy-apis_1.sh
@@ -6,7 +6,7 @@ echo "Usage : deploy-apis.sh to publish apis"
 product=ibm-auto-product_1.0.0
 
 echo "Product name is $product"
-
+export PATH=$PATH:"C:\Users\004DPT744\Downloads\USFB-Install\APIC"
 apic login --accept-license -s "$apic_server_name" -r "$apic_realm" -u "$apic_username" -p "$apic_password"
 # apic create:product --accept-license --title "$product" --apis "$yamlfile"
 # apic draft-products:create --accept-license "$product".yaml --server "$apic_server_name" --org "$apic_org_name"
